01978766189c951e2419f4166bbde98c8665ee41
Alexander Kevin <41278885+aalexanderkevin@users.noreply.github.com>
2019-09-26 11:36:23 +0700

    Initial commit

---
+++ app/code/Midtrans/Snapinst/Controller/Payment/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Midtrans\Snapinst\Controller\Payment;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo '<p>You Did It!</pd>';
+    }
+}
+?>
