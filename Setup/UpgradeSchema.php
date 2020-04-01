e92454d5af5ee5210a359bd6422c0e7a6ced52d0
Xaxxis <zaki.ibrahim@midtrans.com>
2020-04-01 09:52:32 +0700

    UpgradeSchema for adding midtrans_trx_id in sales_order table
    This class running once when run bin/magento setup:upgrade

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ *        c.                                            c.  :.
+ *        E1                                            E.  !)
+ * ::.    E1    !3.        ,5"`'\.;F'`"t.   i.   cF'`'=.E.  !7''`   ;7""   '""!.    ;7"'"!.   ;7'`'*=
+ * ::.    E1    !3.        t.    !)     t   i.  tL      t.  !)     !)     ,,...;)  :1     I.  !t.,
+ * ::.    E1    !3.        t.    !)     t   i.  E.      E.  !)     !L    t'    :1  :1     !)    ``"1.
+ * ::.    E1    !3.        t.    !)     t   i.  '1.,  ,ct.  !1,    !L    1.  ,;31  :1     !) -..   ;7
+ * '      E1    `'         `            `   `     ``'`  `    `'``  `      `''`  `   `     `    ``'`
+ *        E7
+ *
+ * Midtrans Snap Magento 2 Module
+ *
+ * Copyright (c) 2020 Midtrans PT.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Midtrans\Snap\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    const MIDTRANS_TRX_ID = 'midtrans_trx_id';
+
+    /**
+     * Upgrades DB schema for a module, add column midtrans_trx_id
+     * on sales_order
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '2.2.0') < 0) {
+            $tableName = $setup->getTable('sales_order');
+        }
+        if (isset($tableName)) {
+            if ($setup->getConnection()->isTableExists($tableName) == true) {
+                $columns = [
+                    self::MIDTRANS_TRX_ID => [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'length' => 40,
+                        'nullable' => true,
+                        'comment' => 'Midtrans transaction ID',
+                    ],
+                ];
+                $connection = $setup->getConnection();
+                foreach ($columns as $name => $definition) {
+                    $connection->addColumn($tableName, $name, $definition);
+                }
+            }
+        }
+        $setup->endSetup();
+    }
+}
