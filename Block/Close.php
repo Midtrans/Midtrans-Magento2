0d6a624f29e196ef25c19e789f80959da86f01a2
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-01-27 17:35:45 +0700

    remove class cancel and merged with close controller

--- Block/Close.php
+++ Block/Close.php
@@ -2,19 +2,38 @@
 
 namespace Midtrans\Snap\Block;
 
+use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
 class Close extends Template
 {
+    protected $registry;
+
+    /**
+     * Close block constructor.
+     *
+     * @param Context $context
+     * @param Registry $registry
+     * @param array $data
+     */
     public function __construct(
-        Context $context
+        Context $context,
+        Registry $registry,
+        array $data = []
     ) {
-        parent::__construct($context);
+        parent::__construct($context, $data);
+        $this->registry = $registry;
     }
 
-    public function cancelOrders()
+    /**
+     * Function to get orders canceled
+     *
+     * @return mixed|null
+     */
+    public function getOrdersCanceled()
     {
-        return __('Your order has been canceled, because you close payment page. Thank you');
+        $data = $this->registry->registry('orders_canceled');
+        return $data;
     }
 }
